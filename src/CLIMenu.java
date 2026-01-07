5450b60f2704d1a5fab0953fd0694be52cb77b65
Daniel <danielnkurunziza@gmail.com>
2026-01-07 13:03:01 +0000

    feat: Add menu psuedo code

---
+++ src/CLIMenu.java
@@ -0,0 +1,66 @@
+public class CLIMenu {
+//    Acme Bank - Teller System
+    /*
+    1. Find Customer
+    2. Sign Up Customer
+    3. Help
+    4. Exit
+
+    == Find Customer ==
+    Enter CustomerID:
+    -> CUST001 (Unique identifier from table)
+
+    ++ Customer Found ++ [1.0]
+    Customer verified:
+    -- John Smith CUST001
+
+    1. Select this customer
+    2. Re-enter details
+    3. Help
+    0. Cancel operation (Takes you back to main menu)
+
+    -- Customer Not Found -- [2.0]
+    1. Create new customer
+    2. Try again
+    3. Help
+    0. Cancel operation (Takes you back to main menu)
+
+    == Sign Up Customer ==
+
+    Full Name:
+    James Smith
+
+    National Insurance Number:
+    AB123455C
+
+    Photo ID Type (Passport/Driving_License):
+    Passport
+
+    Address ID Type (Utility_Bill/Council_Tax)
+    Utility_Bill
+
+    -- Customer created successfully
+    Customer ID: CUST001
+    Timestamp: 12:51:57
+
+    == Customer: John Smith (CUST001) ==
+
+    1. View Accounts
+    2. Open New Account
+    3. Select Account
+    4. Switch Customer
+    5. View Transactions
+    6. Help
+    0. Exit
+
+    == All Accounts ==
+    No | Type       | Account Number | Sort Code | Balance
+    1   Personal      12345678         60-60-60    $600.00
+    2   ISA           87654321         60-60-70    $1000.00
+
+    0. Back
+
+    == Open New Account ==
+    1. Per
+     */
+}
